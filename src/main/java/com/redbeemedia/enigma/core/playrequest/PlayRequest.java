7aa0a51e9aa54448fc53d084321246aa8cd634a3
Mattias Selin <mattias.selin@eyevinn.se>
2018-11-27 09:41:59 +0100

    Added unit test and made PlayRequest abstract

--- src/main/java/com/redbeemedia/enigma/core/playrequest/PlayRequest.java
+++ src/main/java/com/redbeemedia/enigma/core/playrequest/PlayRequest.java
@@ -1,22 +1,12 @@
 package com.redbeemedia.enigma.core.playrequest;
 
-import com.redbeemedia.enigma.core.error.Error;
 import com.redbeemedia.enigma.core.playable.IPlayable;
 
-public class PlayRequest implements IPlayRequest {
+public abstract class PlayRequest implements IPlayRequest {
     private IPlayable playable;
 
     public PlayRequest(IPlayable playable) {
         this.playable = playable;
-    }
-
-    @Override
-    public void onStarted() {
-    }
-
-    @Override
-    public void onError(Error error) {
-        throw new RuntimeException(error.toString()); //TODO
     }
 
     @Override
