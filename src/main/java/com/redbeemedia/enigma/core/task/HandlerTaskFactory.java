d904e7f2b0549075d73a7fc1e08394fa4f4cbb0e
Mattias Selin <mattias.selin@eyevinn.se>
2019-04-25 15:05:03 +0200

    Repeater utility class

---
+++ src/main/java/com/redbeemedia/enigma/core/task/HandlerTaskFactory.java
@@ -0,0 +1,51 @@
+package com.redbeemedia.enigma.core.task;
+
+import com.redbeemedia.enigma.core.util.IHandler;
+
+public class HandlerTaskFactory implements ITaskFactory {
+    private IHandler handler;
+
+    public HandlerTaskFactory(IHandler handler) {
+        this.handler = handler;
+    }
+
+    @Override
+    public ITask newTask(Runnable runnable) {
+        return new HandlerTask(runnable);
+    }
+
+    private class HandlerTask implements ITask {
+        private volatile boolean canceled = false;
+        private Runnable job;
+
+        public HandlerTask(Runnable job) {
+            this.job = job;
+        }
+
+        @Override
+        public void start() throws TaskException {
+            handler.post(getRunnable());
+        }
+
+        private Runnable getRunnable() {
+            return new Runnable() {
+                @Override
+                public void run() {
+                    if(!canceled) {
+                        job.run();
+                    }
+                }
+            };
+        }
+
+        @Override
+        public void startDelayed(long delayMillis) throws TaskException {
+            handler.postDelayed(getRunnable(), delayMillis);
+        }
+
+        @Override
+        public void cancel(long joinMillis) throws TaskException {
+            canceled = true;
+        }
+    }
+}
