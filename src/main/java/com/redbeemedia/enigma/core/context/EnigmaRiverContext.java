e94f58c05f51de1ca040df5ab98ece5abdb504bd
Mattias Selin <mattias.selin@eyevinn.se>
2019-05-03 12:04:20 +0200

    r1.0.9-BETA-1

--- src/main/java/com/redbeemedia/enigma/core/context/EnigmaRiverContext.java
+++ src/main/java/com/redbeemedia/enigma/core/context/EnigmaRiverContext.java
@@ -59,7 +59,7 @@ public final class EnigmaRiverContext {
         return initializedContext.taskFactory;
     }
 
-
+    public static String TEMP_getRelease() {return "r1.0.9-BETA-1";};
 
     private static void assertInitialized() {
         if(initializedContext == null) {
