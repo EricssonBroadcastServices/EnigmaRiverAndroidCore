d0288bca3ed2c4262335da0db4b920d89023f948
empjenk <empjenk@ericsson.com>
2019-05-15 13:19:18 +0200

    r1.0.15-BETA-11

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Core
 
-The core library, version r1.0.15-BETA-10.
+The core library, version r1.0.15-BETA-11.
 
 See the [tutorials](tutorials/index.md) for more information.
--- src/main/java/com/redbeemedia/enigma/core/context/EnigmaRiverContext.java
+++ src/main/java/com/redbeemedia/enigma/core/context/EnigmaRiverContext.java
@@ -60,7 +60,7 @@ public final class EnigmaRiverContext {
     }
 
     //Version if the core library
-    public static String getVersion() {return "r1.0.15-BETA-10";};
+    public static String getVersion() {return "r1.0.15-BETA-11";};
 
     private static void assertInitialized() {
         if(initializedContext == null) {
