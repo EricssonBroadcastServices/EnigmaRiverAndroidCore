224956f509958317f5422acf173831a989e910b4
empjenk <empjenk@ericsson.com>
2020-04-21 11:30:09 +0000

    r3.0.2

--- src/main/java/com/redbeemedia/enigma/core/context/EnigmaRiverContext.java
+++ src/main/java/com/redbeemedia/enigma/core/context/EnigmaRiverContext.java
@@ -71,7 +71,7 @@ public final class EnigmaRiverContext {
 
     //Version if the core library
     public static String getVersion() {
-        String version = "r3.0.2-BETA-6";
+        String version = "r3.0.2";
         if(version.contains("REPLACE_WITH_RELEASE_VERSION")) {
             return "dev-snapshot-"+BuildConfig.VERSION_NAME;
         } else {
