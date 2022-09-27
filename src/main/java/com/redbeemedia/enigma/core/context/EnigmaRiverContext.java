0b687837114fd5497938138741ab02c72b05b21b
empjenk <empjenk@ericsson.com>
2022-09-27 15:58:14 +0000

    r3.4.7-BETA-5

--- src/main/java/com/redbeemedia/enigma/core/context/EnigmaRiverContext.java
+++ src/main/java/com/redbeemedia/enigma/core/context/EnigmaRiverContext.java
@@ -131,7 +131,7 @@ public final class EnigmaRiverContext {
 
     //Version if the core library
     public static String getVersion() {
-        return "r3.4.7-BETA-4";
+        return "r3.4.7-BETA-5";
     }
 
     private static void assertInitialized() {
